d119d85c223cf20cc06975f3e4dc7aabcc2bae6d
mcthemax777 <mcthemax777@gmail.com>
2023-04-10 21:28:46 +0900

    tt

--- src/ui/components/MoneyDayComponent.js
+++ src/ui/components/MoneyDayComponent.js
@@ -2,7 +2,12 @@ import React, {useContext, useEffect, useRef, useState} from 'react';
 import css from "./MoneyDayComponent.module.css";
 import DayMoneyTaskGroupList from "./DayMoneyTaskGroupList";
 import {MoneyContext} from "../pages/MoneyPage";
-import {convertDateTimeToString} from "../../Defines";
+import {
+    convertDateTimeToString,
+    MONEY_ADD_TYPE_MINUS,
+    MONEY_ADD_TYPE_PLUS,
+    MONEY_MENU_TYPE_CATEGORY
+} from "../../Defines";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
 
@@ -12,12 +17,13 @@ function MoneyDayComponent(props) {
 
     const dayMoneyTimeScroll = useRef();
     const dayMoneyTask = useRef();
-    const { store, } = useContext(MoneyContext);
+    const { store, getMainCategoryNameByNo } = useContext(MoneyContext);
 
     const [totalMoney, setTotalMoney] = useState(0);
     const [minusMoney, setMinusMoney] = useState(0);
     const [plusMoney, setPlusMoney] = useState(0);
     const [groupList, setGroupList] = useState({});
+    const [chartList, setChartList] = useState({});
     const isOverlapTime = (st1, et1, st2, et2) => {
         //겹치지 않으면 return 0
         //1번 시간대가 먼저면 return 1
@@ -38,12 +44,26 @@ function MoneyDayComponent(props) {
         }
     }
 
-
     useEffect(() => {
         setData();
     }, []);
 
-
+    const colorType = [
+        "#000000",
+        "#C0C0C0",
+        "#FF0000",
+        "#800000",
+        "#FFFF00",
+        "#808000",
+        "#00FF00",
+        "#008000",
+        "#00FFFF",
+        "#008080",
+        "#0000FF",
+        "#000080",
+        "#FF00FF",
+        "#800080"
+    ]
 
     const setData = () => {
 
@@ -68,6 +88,8 @@ function MoneyDayComponent(props) {
         let moneyTaskList = props.moneyTaskList;
 
         let resultGroupList = {};
+        let chartDataList = {};
+        let chartLabelList = [];
 
         for(let i = 0; i < moneyTaskList.length; i++) {
 
@@ -78,9 +100,21 @@ function MoneyDayComponent(props) {
             //     continue;
             // }
 
+            //합계 관련 계산
             setTotalMoney(totalMoney + moneyTask.money);
             if(moneyTask.money > 0) setPlusMoney(plusMoney + moneyTask.money);
             else setMinusMoney(minusMoney + moneyTask.money);
+
+            //차트 관련 계산
+            if(moneyTask.categoryType === MONEY_ADD_TYPE_MINUS) {
+                if(chartDataList[moneyTask.mainCategoryNo] === undefined) {
+                    chartDataList[moneyTask.mainCategoryNo] = moneyTask.money;
+                    chartLabelList.push(colorType[chartDataList.length]);
+                } else {
+                    chartDataList[moneyTask.mainCategoryNo] += moneyTask.money;
+                }
+            }
+
 
             taskStartTime.setHours(0);
             taskStartTime.setMinutes(0);
@@ -91,6 +125,9 @@ function MoneyDayComponent(props) {
 
             resultGroupList[taskDate] === undefined ? resultGroupList[taskDate] = [moneyTask] : resultGroupList[taskDate].push(moneyTask);
         }
+
+        console.log("chartDataList");
+        console.log(chartDataList);
 
         setGroupList(resultGroupList);
     }
